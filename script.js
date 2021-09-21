62bf98c31e90741c7a5881e6b0fdb52bb1fcaae6
Kathy Coffee <coffeekl8807@gmail.com>
2021-09-21 16:33:37 -0400

    Complete VariablesExercise with bonus

---
+++ script.js
@@ -0,0 +1,44 @@
+// VariablesExercise
+
+// 1
+// Const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+console.log(num);
+
+// 4
+num = num +7
+console.log(num);
+
+// 5
+num --
+console.log(num);
+
+// 6
+num = "This is not a number";
+console.log(num);
+
+// 7 and 8
+let blank = " "
+console.log(blank);
+
+// 9
+// let blank = null;
+// console.log(null);
+
+// Bonus
+// 1
+const  quarter = 0.25, dime = 0.10, nickel = 0.05, penny = 0.01;
+// 2
+console.log(quarter, dime, nickel, penny);
+
+// 3
+let letters = "qwertyuiop";
+
+// 4
+console.log(letters.length);
+console.log(letters[9]);
